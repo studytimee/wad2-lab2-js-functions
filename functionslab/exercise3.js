ddbadd75ad027cff587a49797a0437e8bc627037
Abid Ali <studytimee@gmail.com>
2022-03-24 15:16:54 +0000

    Exercise 3 - Solution
    Filter uncompleted task and map user id and title of.

--- functionslab/exercise3.js
+++ functionslab/exercise3.js
@@ -3,21 +3,12 @@ fetch("https://jsonplaceholder.typicode.com/todos")
     .then(response => response.json())
     .then(todos => {
 
-        //The Array HOFs can be combined, for example, map over the array produced from the filter function:
-
-        // const finalResult = sourceArray.filter(……).map(…….)
-
-        // The input array for the map is the output array produced by the filter.
-
-        // Create a new file in functionslab, called exercise3.js. Use the same fetch statement as Exercise 1 to request a list of todos.
-
-        // In the body of the fetch, you are required to write the code that creates an array of uncompleted todo objects, where an object contains a userID and todo title. It then displays the array:
-
-        
+        const uncompleteTodo = todos.filter(todo => !todo.completed).map(todo => ({
+            userID: todo.userId, title: todo.title, Completed: todo.completed
+        }))
+        console.log(uncompleteTodo);
 
     })
-
-
     .catch(function (err) {
         console.log(err);
     });

c6bd31d89f3781f127a04dbb78520abf7f5838f2
Abid Ali <studytimee@gmail.com>
2022-03-24 18:59:20 +0000

    Exercise 4 - Solution

--- functionslab/exercise4.js
+++ functionslab/exercise4.js
@@ -2,10 +2,9 @@
 fetch("https://jsonplaceholder.typicode.com/todos")
     .then(response => response.json())
     .then(todos => {
-
-        // Use the same fetch statement as Exercise 1 to request a list (200) of todos.
-
-        // In the body of the fetch, you are required to write the code that computes (and displays) the number of completed todos. Use the Array.reduce HOF in your solution.
+        const totalCompleted = todos.reduce((count, todo) => (todo.completed? count + 1 : count),0
+        )
+        console.log(totalCompleted);
 
     })
     .catch(function (err) {
